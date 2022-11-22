679d82e34d147a1bd31bbd5256664e642eb3a3ce
robonexx <robertwagar@gmail.com>
2022-11-22 16:58:35 +0100

    added win conditionals, and check so the player writes the right words, will add more conditionals and checks

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ console.log('js connected');
 
 let player;
 let bot;
-let result;
-
-player = prompt('Choose rock, paper or scissors').toLowerCase();
 
 let choicesArr = ['rock', 'paper', 'scissors'];
 
@@ -30,9 +27,39 @@ function botChoice() {
 
 botChoice();
 
-if (player === bot) {
-  console.log('It´s a Draw!');
-  alert('It´s a Draw!');
-}
+player = prompt('Choose rock, paper or scissors').toLowerCase();
 
-console.log('Player choice: ' + player + '\nBot choice: ' + bot);
+if (player === 'rock' || player === 'scissors' || player === 'paper') {
+  // result
+  if (player === bot) {
+    console.log(
+      'Player choice: ' + player + '\nBot choice: ' + bot + '\n\nIt´s a Draw!'
+    );
+    alert(
+      'Player choice: ' + player + '\nBot choice: ' + bot + '\nIt´s a Draw!'
+    );
+  } else if (player === 'rock' && bot === 'scissors') {
+    console.log(
+      'Player choice: ' + player + '\nBot choice: ' + bot + '\nYou Win!'
+    );
+    alert('Player choice: ' + player + '\nBot choice: ' + bot + '\nYou Win!');
+  } else if (player === 'scissors' && bot === 'paper') {
+    console.log(
+      'Player choice: ' + player + '\nBot choice: ' + bot + '\nYou Win!'
+    );
+    alert('Player choice: ' + player + '\nBot choice: ' + bot + '\nYou Win!');
+  } else if (player === 'paper' && bot === 'rock') {
+    console.log(
+      'Player choice: ' + player + '\nBot choice: ' + bot + '\nYou Win!'
+    );
+    alert('Player choice: ' + player + '\nBot choice: ' + bot + '\nYou Win!');
+  } else {
+    console.log(
+      'Player choice: ' + player + '\nBot choice: ' + bot + '\nYou Loose!'
+    );
+    alert('Player choice: ' + player + '\nBot choice: ' + bot + '\nYou Loose!');
+  }
+} else {
+  console.log('You must write, rock or paper or scissors!');
+  alert('You must write, rock or paper or scissors!');
+}
