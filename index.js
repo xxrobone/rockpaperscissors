6712970ac89be1fdbed7f3c1af70f43511f452a2
robonexx <robertwagar@gmail.com>
2022-12-06 18:58:11 +0100

    Need to fix the game UI, remove classlist and elements after rounds, maybe with a timeout, Tested a timeout but dident get it right

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ console.log('js connected');
 
 const body = document.body;
 let result = document.querySelector('.alert_box');
+let playerPointsTxt = document.querySelector('#player-points');
+let botPointsTxt = document.querySelector('#bot-points');
 const btn = document.querySelector('.submit_btn');
 
-result.textContent = 'Will show the result';
+const game = document.querySelector('.game');
+
+result.textContent = 'Result';
 // player vs bot & result
 
 let player;
@@ -55,50 +59,129 @@ function playerChoice() {
     console.log('player choice scissors');
     player = 'scissors';
   } else {
-    alert('you must choose');
+    console.log('you must choose');
   }
 }
 
 function playerUI(playerInput) {
-  let output;
+  let playerOutput;
   if (playerInput === 'rock') {
-    output = rock;
+    playerOutput = rock;
   } else if (playerInput === 'paper') {
-    output = paper;
+    playerOutput = paper;
   } else {
-    output = scissors;
+    playerOutput = scissors;
   }
-  return output;
+  return playerOutput;
 }
 
 function botUI(botInput) {
-  let output;
+  let botOutput;
   if (botInput === 'rock') {
-    output = rock;
+    botOutput = rock;
   } else if (botInput === 'paper') {
-    output = paper;
+    botOutput = paper;
   } else {
-    output = scissors;
+    botOutput = scissors;
   }
-  return output;
+  return botOutput;
 }
 
 function gameUI(playerIcon, botIcon) {
-  let game = document.querySelector('.game');
   game.classList.add('game');
   body.appendChild(game);
+
+  game.innerHTML = '';
 
   // adding class to player and bot transform
   playerIcon.classList.add('player_icon');
   botIcon.classList.add('bot_icon');
-  game.append(playerIcon, botIcon);
-}
-gameUI(playerUI(player), botUI(bot));
 
-btn.addEventListener('click', () => {
+  if (playerIcon === botIcon) {
+    playerIcon.classList.add('draw');
+    game.append(playerIcon);
+    /* setTimeout(function () {
+      playerIcon.classList.remove('draw');
+      game.remove(playerIcon);
+      console.log('icons removed');
+    }, 3000); */
+  } else {
+    game.append(playerIcon, botIcon);
+   /*  setTimeout(function () {
+      playerIcon.classList.remove('player_icon');
+      botIcon.classList.remove('bot_icon');
+      game.remove(playerIcon, botIcon);
+      console.log('icons removed');
+    }, 3000); */
+  }
+}
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
   playerChoice();
+  botChoice();
+  gameUI(playerUI(player), botUI(bot));
   console.log('player variable is now: ' + player);
+  gameRound(player, bot);
 });
+
+function gameRound(player, bot) {
+  if (player === 'rock' || player === 'scissors' || player === 'paper') {
+    // result
+    if (player === bot) {
+      result.textContent = 'Its a draw';
+      console.log(
+        '%c Player choice: ' +
+          player +
+          '\nBot choice: ' +
+          bot +
+          '\n\nIt´s a Draw!',
+        'background: #222; color: #ffd500'
+      );
+      /*  alert(
+      'Player choice: ' + player + '\nBot choice: ' + bot + '\nIt´s a Draw!'
+    ); */
+    } else if (player === 'rock' && bot === 'scissors') {
+      result.textContent = 'You Win!';
+      playerPoints++;
+      playerPointsTxt.textContent = playerPoints;
+      console.log(
+        '%c Player choice: ' + player + '\nBot choice: ' + bot + '\nYou Win!',
+        'background: #222; color: #90ee90'
+      );
+      /*  alert('Player choice: ' + player + '\nBot choice: ' + bot + '\nYou Win!'); */
+    } else if (player === 'scissors' && bot === 'paper') {
+      result.textContent = 'You Win!';
+      playerPoints++;
+      playerPointsTxt.textContent = playerPoints;
+      console.log(
+        '%c Player choice: ' + player + '\nBot choice: ' + bot + '\nYou Win!',
+        'background: #222; color: #90ee90'
+      );
+      /* alert('Player choice: ' + player + '\nBot choice: ' + bot + '\nYou Win!'); */
+    } else if (player === 'paper' && bot === 'rock') {
+      result.textContent = 'You Win!';
+      playerPoints++;
+      playerPointsTxt.textContent = playerPoints;
+      console.log(
+        '%c Player choice: ' + player + '\nBot choice: ' + bot + '\nYou Win!',
+        'background: #222; color: #90ee90'
+      );
+      /* alert('Player choice: ' + player + '\nBot choice: ' + bot + '\nYou Win!'); */
+    } else {
+      result.textContent = 'You Loose!';
+      botPoints++;
+      botPointsTxt.textContent = botPoints;
+      console.log(
+        '%c Player choice: ' + player + '\nBot choice: ' + bot + '\nYou Loose!',
+        'background: #222; color: #D2042D'
+      );
+      /*  alert('Player choice: ' + player + '\nBot choice: ' + bot + '\nYou Loose!'); */
+    }
+  } else {
+    console.log('No Choice is made must choose, rock or paper or scissors!');
+  }
+}
 
 // game loop
 /* while (playerPoints <= 5 || botPoints <= 5) {
